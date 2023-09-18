fb6f120a6fd885ba1aec3968d791bbb9efaa07be
bajzc <lzecheng98@gmail.com>
2023-09-18 19:05:36 +0700

    Add section of Building

---
+++ doc/local.mk
@@ -0,0 +1,6 @@
+AM_MAKEINFOFLAGS =                                              \
+  --no-split                                                    \
+  --set-customization-variable=SECTION_NAME_IN_TITLE=true       \
+  --set-customization-variable=AVOID_MENU_REDUNDANCY=true       \
+  --set-customization-variable=ICONS=true
+info_TEXINFOS = doc/MatCal.texi
